ede0d1a43f4572cf30d66b014bfdce0ca0ba0a67
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2024-11-08 18:01:00 +0530

    added local variable

---
+++ pages/variables/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  intro: "Introduction",
+  naming_convention: "Naming Convention",
+  local_variable: "Local Variable",
+  global_variable: "Global Variable",
+  static_variable: "Static Variable",
+};
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,9 +1,19 @@
+import { Terminal } from "lucide-react";
+
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <Terminal style={{ marginRight: "8px" }} />
+      <span style={{ fontWeight: "bold" }}>Docs</span>
+    </div>
+  ),
   project: {
     link: "https://github.com/shuding/nextra",
   },
   footer: false,
   editLink: false,
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
   // ... other theme options
 };
