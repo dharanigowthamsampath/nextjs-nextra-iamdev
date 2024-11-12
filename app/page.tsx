ca991ebfca1d80ebbc933c202ee0444785b66954
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2024-11-12 18:05:40 +0530

    Added component

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,11 @@
-
+import Card from "@/components/card";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
-        <h1 className="text-4xl font-semibold text-center mb-8">Programming Resources</h1>
-        
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-4xl">
-          {/* C Language Card */}
-          <a 
-            href="/C" 
-            className="bg-white/10 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-pointer hover:bg-white/20"
-          >
-            <h2 className="text-xl font-semibold text-center text-white">C Language</h2>
-          </a>
-
-          {/* Java Card */}
-          <a 
-            href="/java" 
-            className="bg-white/10 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-pointer hover:bg-white/20"
-          >
-            <h2 className="text-xl font-semibold text-center text-white">Java</h2>
-          </a>
-
-          {/* Algorithms Card */}
-          <a 
-            href="/algorithm" 
-            className="bg-white/10 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-pointer hover:bg-white/20"
-          >
-            <h2 className="text-xl font-semibold text-center text-white">Algorithms</h2>
-          </a>
-        </div>
-      </main>
-
+  return <main>
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-900">
+      <h1 className="text-xl">Programming Resources</h1>
+      <Card />
     </div>
-  );
+  </main>
 }
+
---
+++ components/card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {};
+
+const Card = (props: Props) => {
+  return (
+    <div className="px-6 py-2 rounded border border-white/20 border-radius-md bg-blue-900">
+      Card
+    </div>
+  );
+};
+
+export default Card;
+
+
+// Token: 6e8a201f6cc177fa7f43b490409e8546dd15682b
+// lns: 1CE23AI024
+// captchacode: VVwkyj
