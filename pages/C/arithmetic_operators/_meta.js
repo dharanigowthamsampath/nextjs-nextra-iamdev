836e16698d6712b8e7a34d7e4ed7d0b851effc78
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2024-11-29 19:47:16 +0530

    Added Arithmetic operator

---
+++ pages/C/arithmetic_operators/_meta.js
@@ -0,0 +1,9 @@
+const meta = {
+  c_addition: "Addition",
+  c_subtraction: "Subtraction",
+  c_multiplication: "Multiplication",
+  c_division: "Division",
+  c_modulus: "Modulus",
+};
+
+export default meta;
