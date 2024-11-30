c39b071ecc3cd2e41d56c089f9aa64f7b0ad0d8e
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2024-11-30 23:29:22 +0530

    Assignment operator updated

--- pages/C/c_assignment_operators/_meta.js
+++ pages/C/c_assignment_operators/_meta.js
@@ -1,5 +1,12 @@
 const meta = {
-  c_simple_assignment: "Simple Assignment (=) Operator",
+  c_simple_assignment: "Simple Assign (=)",
+  c_add_and_assign: "Addition & Assign (+=)",
+  c_subtract_and_assign: "Subtraction & Assign (-=)",
+  c_multiply_and_assign: "Multiplication & Assign (*=)",
+  c_divide_and_assign: "Division & Assign (/=)",
+  c_modulo_and_assign: "Modulo & Assign (%=)",
+  c_left_shift_and_assign: "Left Shift & Assign (<<=)",
+  c_right_shift_and_assign: "Right Shift & Assign (>>=)",
 };
 
 export default meta;
