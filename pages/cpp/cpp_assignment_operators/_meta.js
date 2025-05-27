646cf99fa5e3ad22f0ca8de60586469063be9cf2
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2025-05-27 11:39:19 +0530

    added assignment operator

---
+++ pages/cpp/cpp_assignment_operators/_meta.js
@@ -0,0 +1,13 @@
+const meta = {
+  cpp_equal_operator: "Equal Operator",
+  cpp_plus_equal_operator: "Addition Assignment",
+  cpp_sub_equal_operator: "Subtraction Assignment",
+  cpp_multiple_operator: "Multiple Assignment",
+  cpp_divide_operator: "Divide Assignment",
+  cpp_mod_equal_operator: "Modulus Assignment",
+  cpp_and_equal_operator: "Bitwise AND and assign",
+  cpp_bitwise_or: "Bitwise OR and Assign",
+  cpp_bitwise_xor: "Bitwise XOR and assign",
+};
+
+export default meta;
